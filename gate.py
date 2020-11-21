5f5084417edc4cf0f5c78280905b8916d7ac2691
Wsmith61 <69807232+Wsmith61@users.noreply.github.com>
2020-11-21 11:13:26 -0500

    Update gate.py

--- gate.py
+++ gate.py
@@ -1,7 +1,8 @@
 import schedule
 import time
 #import RPi.GPIO as GPIO
-from datetime import datetime
+from datetime import datetime 
+from gpiozero import LED
 """
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26,GPIO.OUT)
@@ -21,19 +22,20 @@ GPIO.output(20, True)
 GPIO.output(21, True)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 """
+codes = [1234,5678]
+
 bc = 0
 global gateclosed
 gateclosed=False
 global gateopen
 gateopen=False
 #bcflip = 0
-def task():
-    print("do task now")
+led = LED(17)
 
 def opengate():
     global gateclosed
     global gateopen
-    #GPIO.output(26, False)
+    led.on()
     print("opening gate")
     gateopen = True
     gateclosed = False
@@ -41,66 +43,27 @@ def opengate():
 def closegate():
     global gateclosed
     global gateopen
-    #GPIO.output(26, True)
+    led.off()
     print("closing gate")
     gateopen = False
     gateclosed = True
 
-def crestroninput():
-    global input_state
-    global bc
-    #global bcflip
-    #input_state = GPIO.input(4)
-    if input_state == False:
-        #bcflip = 1
-        print('button pressed')
-        opengate()
-        bc += 1
-    else:
-        querrystate()
-def querrystate():
-    global bc
-    time = int(str(datetime.now().time().hour)+str(datetime.now().time().minute))
-    print(time)
-    if time>2200 or time<700:
-        closegate()
-    elif bc>=2:
-        closegate()
-        print("bc")
-        bc = 0
-    else:
-        opengate()
 
-#schedule.every().day.at("07:00").do(opengate)
-#schedule.every().day.at("22:00").do(closegate)
-
-schedule.every().day.at("13:54").do(opengate)
-schedule.every().day.at("13:56").do(closegate)
-
-
-
-try:
-    while True:
-        schedule.run_pending()
-        try:
-            inkink = int(input())
-            if inkink == 1:
-                input_state = True
-            elif inkink == 0:
-                input_state = False
-            crestroninput()
-        except:
-            pass
-        if gateopen == True:
-            print("gate is open")
-        elif gateclosed == True:
-            print("gate is closed")
-        time.sleep(0.2)
+schedule.every().monday.at("07:30").do(opengate)
+schedule.every().monday.at("18:30").do(closegate)
+schedule.every().tuesday.at("07:30").do(opengate)
+schedule.every().tuesday.at("18:30").do(closegate)
+schedule.every().wednesday.at("07:30").do(opengate)
+schedule.every().wednesday.at("18:30").do(closegate)
+schedule.every().thursday.at("07:30").do(opengate)
+schedule.every().thursday.at("18:30").do(closegate)
+schedule.every().friday.at("07:30").do(opengate)
+schedule.every().friday.at("18:30").do(closegate)
 
 while True:
     if gateopen == False:
         codein = input("get code")
-        if codein in codes:
+        if codein in passcodes:
             opengate()
             print("Gate open")
             time.sleep(45)
@@ -108,7 +71,3 @@ while True:
             print("Gate closed")
         else:
             print("wrong code")
-
-finally:
-    #GPIO.cleanup()
-    pass
